8b8f9113498c4dcb83ad71453f44bbb15c9291d0
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-24 23:59:13 +0800

    Add files via upload

---
+++ PrintDemo.java
@@ -0,0 +1,9 @@
+public class PrintDemo {
+    public static void main(String[] args)
+    {
+        System.out.println(10);
+        System.out.printf("%d%n",10);
+        System.out.print(10);
+        System.out.format("%d%n",10);
+    }
+}
