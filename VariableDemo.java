b9589a1ebfb828d16ded76ec36b5059d04665c43
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-25 00:26:03 +0800

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,13 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+//        byte a =0b101;
+////      byte a = 101;
+//        byte a =0x10;
+//        byte a =1_0;
+//        int a =(int)10L;
+//        double a =10;
+//        double a =10L;
+        int a=(int)(10+10L);
+        System.out.println(a);
+    }
+}
