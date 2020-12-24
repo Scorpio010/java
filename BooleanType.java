6499569fca6b76f0a8589ac12139dfc216516667
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-24 23:53:05 +0800

    Add files via upload

---
+++ BooleanType.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class BooleanType {
+    public static void main(String[] args)
+    {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ CharType.java
@@ -0,0 +1,9 @@
+public class CharType {
+    public static void main(String[] args)
+    {
+//        char a = '0';
+//        System.out.println(a);
+          char a = '\n';
+        System.out.println((int)a);
+    }
+}
