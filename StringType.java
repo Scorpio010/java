3cf13143dff368fe57710f5a4f69197b47d46adf
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-26 22:44:08 +0800

    Add files via upload

---
+++ StringType.java
@@ -0,0 +1,15 @@
+import static java.lang.Integer.*;
+
+public class StringType {
+    public static void main(String[] args) {
+//        String a="你好";
+//        String a ="你好"+10;
+//        int a=10;
+//        String as=String.valueOf(a);
+//        System.out.println(as);
+//        String b="10";
+//        int bi=Integer.parselnt(b);
+//        valueOf(b);
+//        System.out.println(bi+10);
+    }
+}
