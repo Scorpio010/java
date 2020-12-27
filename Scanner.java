e13e552d4badbab6da58a2c53fad23ff2ce88839
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-27 15:25:18 +0800

    Add files via upload

---
+++ Scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Scanner {
+    public static void main(String[] args) {
+        java.util.Scanner sca = new java.util.Scanner(System.in);
+        String word1 =sca.next();
+        System.out.println("|"+word1+"|");
+        String word2 = sca.next();
+        System.out.println("|"+word2+"|");
+    }
+}
