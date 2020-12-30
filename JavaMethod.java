c8d5f8f5cfa2fc42da6d22fa8a0416ae3dbd5f05
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2020-12-30 20:53:54 +0800

    Add files via upload

---
+++ JavaMethod.java
@@ -0,0 +1,12 @@
+public class JavaMethod {
+    public static int add(int a,int b){
+        int r = a+b;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(2,5));
+        System.out.println(add(3,4));
+    }
+
+}
