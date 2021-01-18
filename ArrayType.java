0f30980af60d86e05cf4775e99f809009f128ff3
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-18 15:51:14 +0800

    Add files via upload

---
+++ ArrayType.java
@@ -0,0 +1,21 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class ArrayType {
+    public static void main(String[] args) {
+//        int[]  a = {1,2,3,4};//初始化
+//        System.out.println(a);
+//        int[]  a;
+//        a = {1,2,3,4};//不允许
+//        int[]  a=new int[5];
+//         int b = a[3];
+//        System.out.println(b);
+
+//        int[] a = new int[5];
+//        System.out.println(a.length);
+
+//        for (String b : a) {
+//            System.out.println(b);
+//        }
+
+    }
+}
