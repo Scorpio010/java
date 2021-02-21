30f581cc987847717775bef0a4e6082642cc908e
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-02-21 23:55:06 +0800

    Add files via upload

--- startsWith.java
+++ startsWith.java
@@ -1,10 +1,13 @@
 public class startsWith {
     public static void main(String[] args) {
         String s = "Hello World";
-         boolean b = s.startsWith("hel");
+         boolean b = s.startsWith("Hel");
         System.out.println(b);
 
-        boolean c = s.startsWith("hel",0);
+        boolean c = s.startsWith("Hel",0);
         System.out.println(c);
+
+        boolean d =s.startsWith("Hel",1);
+        System.out.println(d);
     }
 }
