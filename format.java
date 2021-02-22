51a67127830d0cb3b3d873c062965ae71fe3d674
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-02-22 22:43:07 +0800

    Add files via upload

---
+++ format.java
@@ -0,0 +1,8 @@
+public class format {
+    public static void main(String[] args) {
+        String s = String.format("Hello %d World %s",18,"xx");
+        System.out.println(s);
+        String t = String.format("My name is %s","liyunzhao");
+        System.out.println(t);
+    }
+}
