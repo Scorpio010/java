d4ba62d3aef339956964ccef2e02719e18cbaf51
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-02-22 22:42:51 +0800

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class  concat {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String t = "world";
+        String r = s.concat(t);
+        System.out.println(r);
+    }
+}
