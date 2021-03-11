7268ddfcfa175ec4faa825501acd58d410be53fd
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-03-11 21:27:08 +0800

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,20 +1,5 @@
+package com.liyunzhao.learn_extends.example;
+
 public class Person {
-    String name;
-    int age;
-    String gender;
-
-    public Person(String name, int age, String gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", gender='" + gender + '\'' +
-                '}';
-    }
+    public String name = "person";
 }
